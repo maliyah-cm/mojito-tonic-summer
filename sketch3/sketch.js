77950091ef964e6185ab871f5e1e6c31b0571b68
mcmiller-web <maliyahmiller.pdx@gmail.com>
2026-05-12 17:37:06 -0700

    push

--- sketch3/sketch.js
+++ sketch3/sketch.js
@@ -63,7 +63,7 @@ function draw() {
   // background('#FF4960');
   let waveScale = 0.3;
   let waveLevel = 3;
-  let sliceH = 4;
+  let sliceH = 2;
   
   let ctx = drawingContext;
   let srcCanvas = mask.canvas;
